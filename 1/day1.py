2979d2ff8d0b6c0b9f7a2255575e7bda423e36a6
sklanier <stevekentlanier@gmail.com>
2021-12-03 13:54:02 -0600

    first pass solution

--- 1/day1.py
+++ 1/day1.py
@@ -1,4 +1,17 @@
-def largerThanLast(nums: list) -> int:
-    pass
+class Solution:
 
-largerThanLast(nums=[])
+    def makeCSV(str: str) -> list:
+        output = []
+        with open('day1.txt') as data:
+            for line in data:
+                 output.append(line.strip())
+
+        return output
+
+    def largerThanLast(nums: list) -> int:
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                count += 1
+
+Solution().largerThanLast(Solution().makeCSV())
